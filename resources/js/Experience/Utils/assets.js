4a4658bdf343c7f281b6ed2b3016c1e3b325ca1b
Guilherme Hiago de Oliveira <gui.hiago.oliveira@gmail.com>
2022-08-19 13:50:32 -0300

    first commit

---
+++ resources/js/Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/SalaAdm.glb"
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/leaguevideo.mp4"
+    }
+]
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import Experience from '@/Experience/Experience.js';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+})
+
+onMounted(() => {
+    const experience = new Experience(document.querySelector(".experience-canvas"));
+})
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div class="experience">
+        <canvas class="experience-canvas">
+
+        </canvas>
+    </div>
+</template>
